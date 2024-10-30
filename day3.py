3dcab44764fbd63ef2b6b40d5aefcbcd2c42432a
jay.choi <jay.choi@kakaocorp.com>
2024-10-30 18:00:55 +0900

    쥬피터 노트 추가

--- day3.py
+++ day3.py
@@ -142,6 +142,11 @@ def relevance_check(state):
     Returns:
         state (dict): Filtered documents and updated web_search state
     """
+    print("---RELEVANCE CHECK---")
+    if "web_search" in state:
+        print(state["web_search"])
+    else:
+        print("no web_search")
     relevance = retrieval_grader.invoke(
         {"question": state["question"], "document": state["documents"]}
     )
@@ -175,23 +180,28 @@ def hallucination_check(state):
     Returns:
         state (dict): return current state
     """
+    # check state has websearch key
     print("---HALLUCINATION CHECK---")
-
-    question = state["question"]
-    documents = state["documents"]
-
-    hallucination = hallucination_grader.invoke(
-        {"documents": documents, "generation": question}
-    )
-
-    score = hallucination["score"]
-    print(hallucination)
-    if score.lower() == "yes":
-        print("success: no hallucination")
+    if "web_search" in state:
+        if state["web_search"] == "yes":
+            print("---HALLUCINATION CHECK: SKIP FOR WEB SEARCH---")
         return state
     else:
-        print("failed: hallucination")
-        exit()
+        question = state["question"]
+        documents = state["documents"]
+        hallucination = hallucination_grader.invoke(
+            {"documents": documents, "generation": question}
+        )
+
+        score = hallucination["score"]
+        print(hallucination)
+        if score.lower() == "yes":
+            print("success: no hallucination")
+            return state
+        else:
+            print("failed: hallucination")
+            # print question and documents
+            exit()
 
 def web_search(state):
     """
@@ -201,11 +211,10 @@ def web_search(state):
         state (dict): The current graph state
 
     Returns:
-        state (dict): Appended web results to documents
+        state (dict): Appended web results to documents, and insert "websearch": "yes" for relevance check
     """
 
     print("---WEB SEARCH---")
-    print(state)
     question = state["question"]
     documents = None
     if "documents" in state:
@@ -213,18 +222,11 @@ def web_search(state):
 
     # Web search
     docs = tavily.search(query=question)['results']
-#    [{'title': 'Where will Lionel Messi play in 2024? Cities, stadiums Inter Miami ...', 'url': 'https://www.sportingnews.com/us/soccer/news/where-lionel-messi-play-2024-inter-miami-cities-road-schedule/23334c5768cebee9021e71d0', 'content': "Here is how Inter Miami's road schedule will look for the coming regular season:\nInter Miami home stadium for 2024 MLS season\nFor their home matches through the 2024 campaign, Inter Miami will once again play at\xa0DRV PNK Stadium in Fort Lauderdale, Florida.\n Cities, stadiums Inter Miami visit on road MLS schedule for new season\nWith Lionel Messi set to embark on his first full season with Inter Miami, fans across the United States will be clamoring to see when the Argentine superstar will visit their city in 2024.\n MLS Season Pass is separate from Apple TV+, meaning those with Apple TV+ would still need an MLS Season Pass subscription to access the complete slate of games, while those without Apple TV+ can still sign up for MLS Season Pass without needing a full Apple TV+ subscription.\n SUBSCRIBE TO MLS SEASON PASS NOW\nApple TV is the official home of the MLS regular season and playoffs, with every match for every team available to stream around the world with no blackouts. How to watch Inter Miami in 2024 MLS season\nLast season, Major League Soccer kicked off a 10-year broadcast rights deal with Apple that sees every single match for the next decade streamed exclusively on Apple's streaming platform.\n", 'score': 0.98612, 'raw_content': None}, {'title': 'Is Lionel Messi playing today? Status for next Inter Miami game in 2024 ...', 'url': 'https://www.sportingnews.com/us/soccer/news/lionel-messi-playing-today-inter-miami-game-2024/129c2c378fee4d1f0102aa9d', 'content': '* Lionel Messi did not participate. Inter Miami schedule for Leagues Cup. The 2024 Leagues Cup is scheduled to begin on July 26, running for a month while the MLS season pauses play.. The final ...', 'score': 0.98209, 'raw_content': None}, {'title': 'Lionel Messi joins Inter Miami: Schedule, MLS tickets to see him play', 'url': 'https://www.usatoday.com/story/sports/mls/2023/06/07/lionel-messi-inter-miami-schedule-tickets/70299298007/', 'content': 'Lionel Messi joins Inter Miami: Full schedule, MLS tickets to see Messi play in US\nLionel Messi\xa0is taking his talents to South Beach.\nMessi,\xa0the 2022 World Cup champion, announced on Wednesday that he will join Major League Soccer\'s Inter Miami CF, a pro soccer club owned by David Beckham, after exiting Ligue 1\'s Paris Saint-Germain following two seasons.\n Tickets to Inter Miami\'s game on June 10 range from $40-$55, but the price tag to see Inter Miami play LigaMX\'s Cruz Azul on July 21 soared to $495 in anticipation of what\'s expected to be Messi\'s first home game, TicketSmarter CEO Jeff Goodman told USA TODAY Sports.\n Each team will play a minimum of two games in the group stage, similar to the World Cup format, with the possibility of more games if the team advances to the knockout rounds.\n "\nAccording to Goodman, nearly 2,000 Inter Miami tickets sold on TicketSmarter the day of Messi\'s announcement Wednesday, compared to under 50 tickets being sold on the platform over the weekend.\n If the Barcelona thing didn\'t work out, I wanted to leave Europe, get out of the spotlight and think more of my family.', 'score': 0.97895, 'raw_content': None}, {'title': "Lionel Messi's 2023 Inter Miami schedule: Every match in MLS, Leagues ...", 'url': 'https://www.sportingnews.com/us/soccer/news/lionel-messi-2023-inter-miami-schedule/d3buao2mhfp7uculkdz3nsc4', 'content': "MORE:\xa0Trophies that Lionel Messi can win with Inter Miami in USA\nLeagues Cup\nIn his first three matches with Inter Miami, Lionel Messi lifted the club into the Leagues Cup Round of 16 thanks to three straight home wins that he helped orchestrate.\n Edition\nLionel Messi's 2023 Inter Miami schedule: Every match in MLS, Leagues Cup and U.S. Open Cup\nLionel Messi is taking North America by storm after scoring in his first three matches for his new club Inter Miami CF.\n MORE: Messi's Miami apartment | Messi's wife & family | Messi's net worth\nLionel Messi, Inter Miami 2023 schedule\nBelow are the remaining games for Inter Miami that Messi will be a part of. MLS\nAfter the Leagues Cup is out of the way, Inter Miami will have 12 MLS matchdays left in a bid to reach the MLS Cup playoffs.\n Inter Miami can still make MLS playoffs\xa0with Lionel Messi\nU.S. Open Cup\nInter Miami reached the semifinal of the competition before Messi and friends joined.", 'score': 0.97298, 'raw_content': None}, {'title': 'Messi, Argentina to play in Chicago, DC before Copa America: More info', 'url': 'https://www.usatoday.com/story/sports/soccer/2024/05/20/messi-argentina-to-play-in-chicago-dc-before-copa-america-more-info/73770204007/', 'content': "1:00. World Cup champion Lionel Messi will participate in two Argentina friendlies early next month before Copa América begins June 20. Messi was officially named to Argentina's 29-man roster ...", 'score': 0.97096, 'raw_content': None}]
-#
-#
 
     web_results = "\n".join([d["content"] for d in docs])
     web_results = Document(page_content=web_results)
-    if documents is not None:
-        documents.append(web_results)
-    else:
-        documents = [web_results]
-    return {"documents": documents, "question": question}
-
+    documents = [web_results]
+    return {"documents": documents, "question": question, "web_search": "yes"}
 
 ### Edges
 
@@ -259,12 +261,12 @@ def decide_to_generate_or_websearch(state):
 workflow = StateGraph(GraphState)
 
 # Define the nodes
-# 1. retrieve
 workflow.add_node("retrieve", retrieve)  # retrieve
 workflow.set_entry_point("retrieve")
 workflow.add_node("relevance_check", relevance_check)
 workflow.add_edge("retrieve", "relevance_check")
 workflow.add_node("websearch", web_search)  # web search
+workflow.add_edge("websearch", "relevance_check")
 
 # relevance check가 yes인 경우인 경우 websearch 호출
 workflow.add_conditional_edges(
@@ -279,42 +281,46 @@ workflow.add_conditional_edges(
 workflow.add_node("generate", generate)
 workflow.add_node("hallucination_check", hallucination_check)
 workflow.add_edge("generate", "hallucination_check")
-# workflow.add_node("grade_documents", grade_documents)  # grade documents
-# workflow.add_node("generate", generate)  # generatae
-
-# # Build graph
-# workflow.set_conditional_entry_point(
-#     route_question,
-#     {
-#         "websearch": "websearch",
-#         "vectorstore": "retrieve",
-#     },
-# )
-
-# workflow.add_edge("retrieve", "grade_documents")
-# workflow.add_conditional_edges(
-#     "grade_documents",
-#     decide_to_generate,
-#     {
-#         "websearch": "websearch",
-#         "generate": "generate",
-#     },
-# )
-# workflow.add_edge("websearch", "generate")
-# workflow.add_conditional_edges(
-#     "generate",
-#     grade_generation_v_documents_and_question,
-#     {
-#         "not supported": "generate",
-#         "useful": END,
-#         "not useful": "websearch",
-#     },
-# )
-
 # Compile
 app = workflow.compile()
 
-# Test
+# import curve style
+from langchain_core.runnables.graph import CurveStyle
+from langchain_core.runnables.graph import NodeStyles
+from langchain_core.runnables.graph import MermaidDrawMethod
+
+import nest_asyncio
+
+nest_asyncio.apply()  # Required for Jupyter Notebook to run async functions
+
+# write image file
+image = app.get_graph().draw_mermaid_png(
+    curve_style=CurveStyle.LINEAR,
+    node_colors=NodeStyles(first="#ffdfba", last="#baffc9", default="#fad7de"),
+    wrap_label_n_words=9,
+    output_file_path="graph.png",
+    draw_method=MermaidDrawMethod.PYPPETEER,
+    background_color="white",
+    padding=10,
+)
+
+with open("graph.png", "wb") as f:
+    f.write(image)
+
+# display(
+#     Image(
+#         app.get_graph().draw_mermaid_png(
+#             curve_style=CurveStyle.LINEAR,
+#             node_colors=NodeStyles(first="#ffdfba", last="#baffc9", default="#fad7de"),
+#             wrap_label_n_words=9,
+#             output_file_path=None,
+#             draw_method=MermaidDrawMethod.PYPPETEER,
+#             background_color="white",
+#             padding=10,
+#         )
+#     )
+# )
+
 # command line에서 query를 입력
 import sys
 query = sys.argv[1]
@@ -324,4 +330,7 @@ outputs = app.stream(inputs)
 for output in outputs:
     for key, value in output.items():
         pprint(f"Finished running: {key}:")
-pprint(value["documents"])
+
+# print documents for generated answer
+
+pprint(value["generation"])
