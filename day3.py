8eed6a54ec34b6c2b617b69a9746480e225b95eb
jay.choi <jay.choi@kakaocorp.com>
2024-10-30 15:22:22 +0900

    day3 run sample
    retrive 만 실행
    
    retrieve > relevance_check 노드 연결

--- day3.py
+++ day3.py
@@ -1,1 +1,395 @@
 #! /usr/bin/env python
+
+from tavily import TavilyClient
+from langchain_community.document_loaders import WebBaseLoader
+from langchain_openai import OpenAIEmbeddings
+from langchain_community.vectorstores import Chroma
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_core.output_parsers import JsonOutputParser
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_openai import ChatOpenAI
+import os
+import json
+
+llm = ChatOpenAI(model="gpt-4o-mini", temperature = 0)
+tavily = TavilyClient(api_key=os.environ["TAVILY_API_KEY"])
+
+urls = [
+    "https://lilianweng.github.io/posts/2023-06-23-agent/",
+    "https://lilianweng.github.io/posts/2023-03-15-prompt-engineering/",
+    "https://lilianweng.github.io/posts/2023-10-25-adv-attack-llm/",
+]
+
+docs_list = WebBaseLoader(urls).load()
+
+text_splitter = RecursiveCharacterTextSplitter.from_tiktoken_encoder(
+    chunk_size=250, chunk_overlap=0
+)
+doc_splits = text_splitter.split_documents(docs_list)
+
+# Add to vectorDB
+vectorstore = Chroma.from_documents(
+    documents=doc_splits,
+    collection_name="rag-chroma",
+    embedding = OpenAIEmbeddings(model="text-embedding-3-small")
+)
+retriever = vectorstore.as_retriever()
+
+from pprint import pprint
+from typing import List
+
+from langchain_core.documents import Document
+from typing_extensions import TypedDict
+
+from langgraph.graph import END, StateGraph
+
+### Retrieval Grader
+system = """
+    당신은 사용자 질문에 대한 검색된 문서의 관련성을 평가하는 채점자입니다. 
+    문서가 사용자 질문과 관련된 키워드를 포함하고 있다면 관련성이 있다고 평가하세요. 엄격한 테스트일 필요는 없습니다. 목표는 잘못된 검색 결과를 걸러내는 것입니다.
+    문서가 질문과 관련이 있는지 'yes' 또는 'no'로 이진 점수를 주세요.
+    점수를 'relevance' 키 하나만 있는 JSON으로 제공하고, 설명이나 서문은 포함하지 마세요.
+    """
+
+prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system", system),
+        ("human", "question: {question}\n\n document: {document} "),
+    ]
+)
+
+retrieval_grader = prompt | llm | JsonOutputParser()
+question = "What is prompt?"
+
+### Generate
+
+system = """
+    당신은 질문-답변 작업을 위한 도우미입니다.
+    제공된 검색 컨텍스트를 사용하여 질문에 답하세요. 답을 모르는 경우 모른다고 말하세요.
+    최대 3문장을 사용하고 답변을 간단하게 유지하세요
+    """
+
+prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system", system),
+        ("human", "question: {question}\n\n context: {context} "),
+    ]
+)
+
+# Chain
+rag_chain = prompt | llm | StrOutputParser()
+
+### Hallucination Grader
+system = """
+    당신은 답변이 주어진 사실들에 근거하고 있는지/지원되는지 평가하는 채점자입니다. 
+    답변이 사실들에 근거하고 있는지/지원되는지 'yes' 또는 'no'로 이진 점수를 주세요. 
+    점수를 'score' 키 하나만 있는 JSON으로 제공하고, 설명이나 서문은 포함하지 마세요.
+    """
+
+prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system", system),
+        ("human", "documents: {documents}\n\n answer: {generation} "),
+    ]
+)
+
+hallucination_grader = prompt | llm | JsonOutputParser()
+
+### Answer Grader
+prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system", system),
+        ("human", "{question}"),
+    ]
+)
+
+# Prompt
+system = """
+    당신은 답변이 질문 해결에 유용한지 평가하는 채점자입니다.
+    답변이 질문 해결에 유용한지 'yes' 또는 'no'로 이진 점수를 주세요. 
+    점수를 'score' 키 하나만 있는 JSON으로 제공하고, 설명이나 서문은 포함하지 마세요.
+    """
+
+prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system", system),
+        ("human", "question: {question}\n\n answer: {generation} "),
+    ]
+)
+
+answer_grader = prompt | llm | JsonOutputParser()
+
+class GraphState(TypedDict):
+    """
+    Represents the state of our graph.
+
+    Attributes:
+        question: question
+        generation: LLM generation
+        web_search: whether to add search
+        documents: list of documents
+    """
+
+    question: str
+    generation: str
+    web_search: str
+    relevance: str
+    documents: List[str]
+
+
+### Nodes
+
+def retrieve(state):
+    """
+    Retrieve documents from vectorstore
+
+    Args:
+        state (dict): The current graph state
+
+    Returns:
+        state (dict): New key added to state, documents, that contains retrieved documents
+    """
+    print("---RETRIEVE---")
+    question = state["question"]
+
+    # Retrieval
+    documents = retriever.invoke(question)
+    print(question)
+    print(documents)
+    return {"documents": documents, "question": question}
+
+def relevance_check(state):
+    """
+    Check if the retrieved documents are relevant to the question
+
+    Args:
+        state (dict): The current graph state
+
+    Returns:
+        state (dict): Filtered documents and updated web_search state
+    """
+    relevance = retrieval_grader.invoke(
+        {"question": state["question"], "document": state["documents"]}
+    )
+    return {"documents": state["documents"], "question": state["question"], "relevance": relevance}
+
+def generate(state):
+    """
+    Generate answer using RAG on retrieved documents
+
+    Args:
+        state (dict): The current graph state
+
+    Returns:
+        state (dict): New key added to state, generation, that contains LLM generation
+    """
+    print("---GENERATE---")
+    question = state["question"]
+    documents = state["documents"]
+
+    # RAG generation
+    generation = rag_chain.invoke({"context": documents, "question": question})
+    return {"documents": documents, "question": question, "generation": generation}
+
+
+def grade_documents(state):
+    """
+    Determines whether the retrieved documents are relevant to the question
+    If any document is not relevant, we will set a flag to run web search
+
+    Args:
+        state (dict): The current graph state
+
+    Returns:
+        state (dict): Filtered out irrelevant documents and updated web_search state
+    """
+
+    print("---CHECK DOCUMENT RELEVANCE TO QUESTION---")
+    question = state["question"]
+    documents = state["documents"]
+
+    # Score each doc
+    filtered_docs = []
+    web_search = "No"
+    for d in documents:
+        score = retrieval_grader.invoke(
+            {"question": question, "document": d.page_content}
+        )
+        grade = score["score"]
+        # Document relevant
+        if grade.lower() == "yes":
+            print("---GRADE: DOCUMENT RELEVANT---")
+            filtered_docs.append(d)
+        # Document not relevant
+        else:
+            print("---GRADE: DOCUMENT NOT RELEVANT---")
+            # We do not include the document in filtered_docs
+            # We set a flag to indicate that we want to run web search
+            web_search = "Yes"
+            continue
+    return {"documents": filtered_docs, "question": question, "web_search": web_search}
+
+
+def web_search(state):
+    """
+    Web search based based on the question
+
+    Args:
+        state (dict): The current graph state
+
+    Returns:
+        state (dict): Appended web results to documents
+    """
+
+    print("---WEB SEARCH---")
+    print(state)
+    question = state["question"]
+    documents = None
+    if "documents" in state:
+      documents = state["documents"]
+
+    # Web search
+    docs = tavily.search(query=question)['results']
+#    [{'title': 'Where will Lionel Messi play in 2024? Cities, stadiums Inter Miami ...', 'url': 'https://www.sportingnews.com/us/soccer/news/where-lionel-messi-play-2024-inter-miami-cities-road-schedule/23334c5768cebee9021e71d0', 'content': "Here is how Inter Miami's road schedule will look for the coming regular season:\nInter Miami home stadium for 2024 MLS season\nFor their home matches through the 2024 campaign, Inter Miami will once again play at\xa0DRV PNK Stadium in Fort Lauderdale, Florida.\n Cities, stadiums Inter Miami visit on road MLS schedule for new season\nWith Lionel Messi set to embark on his first full season with Inter Miami, fans across the United States will be clamoring to see when the Argentine superstar will visit their city in 2024.\n MLS Season Pass is separate from Apple TV+, meaning those with Apple TV+ would still need an MLS Season Pass subscription to access the complete slate of games, while those without Apple TV+ can still sign up for MLS Season Pass without needing a full Apple TV+ subscription.\n SUBSCRIBE TO MLS SEASON PASS NOW\nApple TV is the official home of the MLS regular season and playoffs, with every match for every team available to stream around the world with no blackouts. How to watch Inter Miami in 2024 MLS season\nLast season, Major League Soccer kicked off a 10-year broadcast rights deal with Apple that sees every single match for the next decade streamed exclusively on Apple's streaming platform.\n", 'score': 0.98612, 'raw_content': None}, {'title': 'Is Lionel Messi playing today? Status for next Inter Miami game in 2024 ...', 'url': 'https://www.sportingnews.com/us/soccer/news/lionel-messi-playing-today-inter-miami-game-2024/129c2c378fee4d1f0102aa9d', 'content': '* Lionel Messi did not participate. Inter Miami schedule for Leagues Cup. The 2024 Leagues Cup is scheduled to begin on July 26, running for a month while the MLS season pauses play.. The final ...', 'score': 0.98209, 'raw_content': None}, {'title': 'Lionel Messi joins Inter Miami: Schedule, MLS tickets to see him play', 'url': 'https://www.usatoday.com/story/sports/mls/2023/06/07/lionel-messi-inter-miami-schedule-tickets/70299298007/', 'content': 'Lionel Messi joins Inter Miami: Full schedule, MLS tickets to see Messi play in US\nLionel Messi\xa0is taking his talents to South Beach.\nMessi,\xa0the 2022 World Cup champion, announced on Wednesday that he will join Major League Soccer\'s Inter Miami CF, a pro soccer club owned by David Beckham, after exiting Ligue 1\'s Paris Saint-Germain following two seasons.\n Tickets to Inter Miami\'s game on June 10 range from $40-$55, but the price tag to see Inter Miami play LigaMX\'s Cruz Azul on July 21 soared to $495 in anticipation of what\'s expected to be Messi\'s first home game, TicketSmarter CEO Jeff Goodman told USA TODAY Sports.\n Each team will play a minimum of two games in the group stage, similar to the World Cup format, with the possibility of more games if the team advances to the knockout rounds.\n "\nAccording to Goodman, nearly 2,000 Inter Miami tickets sold on TicketSmarter the day of Messi\'s announcement Wednesday, compared to under 50 tickets being sold on the platform over the weekend.\n If the Barcelona thing didn\'t work out, I wanted to leave Europe, get out of the spotlight and think more of my family.', 'score': 0.97895, 'raw_content': None}, {'title': "Lionel Messi's 2023 Inter Miami schedule: Every match in MLS, Leagues ...", 'url': 'https://www.sportingnews.com/us/soccer/news/lionel-messi-2023-inter-miami-schedule/d3buao2mhfp7uculkdz3nsc4', 'content': "MORE:\xa0Trophies that Lionel Messi can win with Inter Miami in USA\nLeagues Cup\nIn his first three matches with Inter Miami, Lionel Messi lifted the club into the Leagues Cup Round of 16 thanks to three straight home wins that he helped orchestrate.\n Edition\nLionel Messi's 2023 Inter Miami schedule: Every match in MLS, Leagues Cup and U.S. Open Cup\nLionel Messi is taking North America by storm after scoring in his first three matches for his new club Inter Miami CF.\n MORE: Messi's Miami apartment | Messi's wife & family | Messi's net worth\nLionel Messi, Inter Miami 2023 schedule\nBelow are the remaining games for Inter Miami that Messi will be a part of. MLS\nAfter the Leagues Cup is out of the way, Inter Miami will have 12 MLS matchdays left in a bid to reach the MLS Cup playoffs.\n Inter Miami can still make MLS playoffs\xa0with Lionel Messi\nU.S. Open Cup\nInter Miami reached the semifinal of the competition before Messi and friends joined.", 'score': 0.97298, 'raw_content': None}, {'title': 'Messi, Argentina to play in Chicago, DC before Copa America: More info', 'url': 'https://www.usatoday.com/story/sports/soccer/2024/05/20/messi-argentina-to-play-in-chicago-dc-before-copa-america-more-info/73770204007/', 'content': "1:00. World Cup champion Lionel Messi will participate in two Argentina friendlies early next month before Copa América begins June 20. Messi was officially named to Argentina's 29-man roster ...", 'score': 0.97096, 'raw_content': None}]
+#
+#
+
+    web_results = "\n".join([d["content"] for d in docs])
+    web_results = Document(page_content=web_results)
+    if documents is not None:
+        documents.append(web_results)
+    else:
+        documents = [web_results]
+    return {"documents": documents, "question": question}
+
+
+### Edges
+
+def decide_to_generate(state):
+    """
+    Determines whether to generate an answer, or add web search
+
+    Args:
+        state (dict): The current graph state
+
+    Returns:
+        str: Binary decision for next node to call
+    """
+
+    print("---ASSESS GRADED DOCUMENTS---")
+    state["question"]
+    web_search = state["web_search"]
+    state["documents"]
+
+    if web_search == "Yes":
+        # All documents have been filtered check_relevance
+        # We will re-generate a new query
+        print(
+            "---DECISION: ALL DOCUMENTS ARE NOT RELEVANT TO QUESTION, INCLUDE WEB SEARCH---"
+        )
+        return "websearch"
+    else:
+        # We have relevant documents, so generate answer
+        print("---DECISION: GENERATE---")
+        return "generate"
+
+
+### Conditional edge
+
+
+def grade_generation_v_documents_and_question(state):
+    """
+    Determines whether the generation is grounded in the document and answers question.
+
+    Args:
+        state (dict): The current graph state
+
+    Returns:
+        str: Decision for next node to call
+    """
+
+    print("---CHECK HALLUCINATIONS---")
+    question = state["question"]
+    documents = state["documents"]
+    generation = state["generation"]
+
+    score = hallucination_grader.invoke(
+        {"documents": documents, "generation": generation}
+    )
+    grade = score["score"]
+
+    # Check hallucination
+    if grade == "yes":
+        print("---DECISION: GENERATION IS GROUNDED IN DOCUMENTS---")
+        # Check question-answering
+        print("---GRADE GENERATION vs QUESTION---")
+        score = answer_grader.invoke({"question": question, "generation": generation})
+        grade = score["score"]
+        if grade == "yes":
+            print("---DECISION: GENERATION ADDRESSES QUESTION---")
+            return "useful"
+        else:
+            print("---DECISION: GENERATION DOES NOT ADDRESS QUESTION---")
+            return "not useful"
+    else:
+        pprint("---DECISION: GENERATION IS NOT GROUNDED IN DOCUMENTS, RE-TRY---")
+        return "not supported"
+
+
+workflow = StateGraph(GraphState)
+
+# Define the nodes
+# 1. retrieve
+workflow.add_node("retrieve", retrieve)  # retrieve
+
+workflow.set_entry_point("retrieve")
+
+workflow.add_node("relevance_check", relevance_check)
+
+workflow.add_edge("retrieve", "relevance_check")
+
+# workflow.add_node("websearch", web_search)  # web search
+# workflow.add_node("grade_documents", grade_documents)  # grade documents
+# workflow.add_node("generate", generate)  # generatae
+
+# # Build graph
+# workflow.set_conditional_entry_point(
+#     route_question,
+#     {
+#         "websearch": "websearch",
+#         "vectorstore": "retrieve",
+#     },
+# )
+
+# workflow.add_edge("retrieve", "grade_documents")
+# workflow.add_conditional_edges(
+#     "grade_documents",
+#     decide_to_generate,
+#     {
+#         "websearch": "websearch",
+#         "generate": "generate",
+#     },
+# )
+# workflow.add_edge("websearch", "generate")
+# workflow.add_conditional_edges(
+#     "generate",
+#     grade_generation_v_documents_and_question,
+#     {
+#         "not supported": "generate",
+#         "useful": END,
+#         "not useful": "websearch",
+#     },
+# )
+
+# Compile
+app = workflow.compile()
+
+# Test
+
+inputs = {"question": "What is prompt?"}
+outputs = app.stream(inputs)
+for output in outputs:
+    for key, value in output.items():
+        pprint(f"Finished running: {key}:")
+pprint(value)
